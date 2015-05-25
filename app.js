be7a556cff223834f659821e4a47bcb1a8a40265
Keith Rogers <git@keithrogers.eu>
2015-05-25 13:26:09 +0100

    Add some tests

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+	search: function () {}
+};
---
+++ tests/app.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var expect = require('chai').expect;
+var app = require('../app');
+
+describe('Clavivox', function () {
+
+	it('should export an object', function () {
+		expect(app).to.be.an('object');
+	});
+
+	it('should have a search method', function () {
+		expect(app.search).to.be.an('function');
+	});
+});
